81cf2257760faa4bb39f64afe63d13706fa8a8a7
blancojohn <142819007+blancojohn@users.noreply.github.com>
2024-07-22 17:09:02 +0000

    Reforzando en funciones

--- exercises/09-Array-Methods/app.js
+++ exercises/09-Array-Methods/app.js
@@ -2,6 +2,8 @@ let names = ['John', 'Kenny', 'Tom', "Bob", 'Dilan'];
 
 function sortNames(arr) {
   // Your code goes here
+  arr.sort()
+  return arr
   
 }
 
--- exercises/10-Remove-vowels/app.js
+++ exercises/10-Remove-vowels/app.js
@@ -1,6 +1,10 @@
 // Your code goes here
+const rapid= (string)=> {for (let i= 0; string.length; i++){
+    let notVocals= string.replace(/[aeiou]/g, "").toUpperCase()
+    return notVocals
+}};
 
 
 // Work above this line; do not change code below
-let str = "John";
+let str = "Pamela";
 console.log(rapid(str));
